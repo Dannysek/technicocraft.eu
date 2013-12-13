eb249e82d9b204f775e4a0aa6b5d391c246f233e
Dannysek <admin@zsjzemana.eu>
2013-12-13 20:27:53 +0100

    Added .htaccess, snow.js (Later) and domov.php (SK: Domov ENG: Home).

---
+++ domov.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title><?php echo "$title"; ?></title>
+	</head>
+	<body>
+		<div class="header">
+			<div class="container_header">
+				<div class="background_mc"></div>
+				<div class="menu">
+					<ul>
+						<li><a href="domov/"><span>Domov</span></a></li>
+						<li><a href="kontakt/"><span>Kontakt</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="page">
+			<div class="container_page">
+				
+			</div>
+		</div>
+		<div class="footer">
+			<div class="container_footer">
+				<div class="logo"></div>
+				<div class="copy">
+					<div class="container_copy">
+						<p>
+							&copy; 2013 Powered by <a href="http://dannysek.eu/dannycms/">DannyCMS</a> 
+							Theme by <a href="http://dannysek.eu">Dannysek</a>
+						</p>
+					</div>
+				</div>
+				<div class="on_working">
+					<div class="container_on_working">
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
